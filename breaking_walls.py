10d382f7de90ab966081143c269a0c6d4ce1c59b
student236712 <236712@student.pwr.edu.pl>
2020-11-11 13:19:51 +0100

    Create class ball with move method

--- breaking_walls.py
+++ breaking_walls.py
@@ -7,7 +7,7 @@ HEIGHT = 700
 
 tk = Tk()
 canvas = Canvas(tk, width=WIDTH, height=HEIGHT)
-tk.title("Ball breaking the walls")
+tk.title("Balls breaking the walls")
 canvas.pack()
 
 BALL_SIZE = 35
@@ -15,28 +15,44 @@ BALL_COLOUR = "steel blue"
 start_X_1_position = random.randrange(0, WIDTH - BALL_SIZE)
 start_Y_1_position = random.randrange(0, HEIGHT - BALL_SIZE)
 
-ball = canvas.create_oval(start_X_1_position, start_Y_1_position, start_X_1_position + BALL_SIZE,
-                          start_Y_1_position + BALL_SIZE, fill=BALL_COLOUR)
-xspeed = random.randrange(-12, 12, 3)
-yspeed = random.randrange(-12, 12, 3)
+
+class Ball:
+    def __init__(self, start_x_1_position, start_y_1_position, color, size):
+        self.color = color
+        self.shape = canvas.create_oval(start_x_1_position, start_y_1_position, start_x_1_position + size,
+                                        start_y_1_position + size, fill=color)
+        self.xspeed = random.randrange(-10, 10)
+        self.yspeed = random.randrange(-10, 10)
+
+    def move(self):
+        canvas.move(self.shape, self.xspeed, self.yspeed)
+        pos = canvas.coords(self.shape)
+        if pos[3] >= HEIGHT:
+            canvas.delete(self.shape)
+            # Create oval with coordinates x1,y1,x2,y2."""
+            self.shape = canvas.create_oval(pos[0], 0, pos[2], BALL_SIZE, fill=self.color)
+        if pos[1] <= 0:
+            canvas.delete(self.shape)
+            self.shape = canvas.create_oval(pos[0], HEIGHT, pos[2], HEIGHT - BALL_SIZE, fill=self.color)
+        if pos[2] >= WIDTH:
+            canvas.delete(self.shape)
+            self.shape = canvas.create_oval(0, pos[1], BALL_SIZE, pos[3], fill=self.color)
+        if pos[0] <= 0:
+            canvas.delete(self.shape)
+            self.shape = canvas.create_oval(WIDTH, pos[1], WIDTH - BALL_SIZE, pos[3], fill=self.color)
+
+
+BALL_SIZE = 35
+
+balls = []
+balls.append(Ball(random.randrange(0, WIDTH - BALL_SIZE), random.randrange(0, HEIGHT - BALL_SIZE), "steel blue",
+                  BALL_SIZE))
+balls.append(Ball(random.randrange(0, WIDTH - BALL_SIZE), random.randrange(0, HEIGHT - BALL_SIZE), "maroon", BALL_SIZE))
+balls.append(Ball(random.randrange(0, WIDTH - BALL_SIZE), random.randrange(0, HEIGHT - BALL_SIZE), "salmon1", BALL_SIZE))
 
 while True:
-    canvas.move(ball, xspeed, yspeed)
-    # pos = [left,top,right,bottom]
-    pos = canvas.coords(ball)
-    if pos[3] >= HEIGHT:
-        canvas.delete(ball)
-        # Create oval with coordinates x1,y1,x2,y2."""
-        ball = canvas.create_oval(pos[0], 0, pos[2], BALL_SIZE, fill=BALL_COLOUR)
-    if pos[1] <= 0:
-        canvas.delete(ball)
-        ball = canvas.create_oval(pos[0], HEIGHT, pos[2], HEIGHT - BALL_SIZE, fill=BALL_COLOUR)
-    if pos[2] >= WIDTH:
-        canvas.delete(ball)
-        ball = canvas.create_oval(0, pos[1], BALL_SIZE, pos[3], fill=BALL_COLOUR)
-    if pos[0] <= 0:
-        canvas.delete(ball)
-        ball = canvas.create_oval(WIDTH, pos[1], WIDTH - BALL_SIZE, pos[3], fill=BALL_COLOUR)
+    for ball in balls:
+        ball.move()
     tk.update()
     time.sleep(0.01)
 
